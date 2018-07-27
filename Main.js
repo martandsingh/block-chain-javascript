2d409619abf4aa80e137c598223271da264b0979
Martand Singh <martandsays@gmail.com>
2018-07-27 09:38:33 +0530

    Comments added

--- Main.js
+++ Main.js
@@ -1,3 +1,9 @@
+/**
+ * Author : Martand Singh
+ * Date : 27/July/2018
+ * Scope : Basic Block Chain demo
+ */
+
 const SHA256 = require('crypto-js/sha256')
 
 class Block{
@@ -10,10 +16,12 @@ class Block{
         this.nonce = 0
     }
 
+    //Calculate the hashcode for block
     calculateHash(){
         return SHA256(this.index + this.previoushash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
 
+    //Calculating prepended zero difficulty
     mineBlock(difficulty){
         while(this.hash.toString().substring(0, difficulty) !== Array(difficulty + 1).join("0") ){
             this.nonce = this.nonce++
@@ -28,14 +36,17 @@ class BlockChain{
         this.difficulty = 2;
     }
 
+    //Create very first block, which is called genesys block
     createGenesisBlock(){
-        return new Block(0, '2018-07-27', 'Martand Singh', '0')
+        return new Block(0, '2018-07-27', {data : 100}, '0')
     }
 
+    //Get the latest or last block of the block chain
     getLatestBlock(){
         return this.chain[this.chain.length - 1]
     }
 
+    //create a new block
     addBlock(newBLock){
         newBLock.previoushash = this.getLatestBlock().hash;
         newBLock.hash = newBLock.calculateHash();
@@ -43,6 +54,7 @@ class BlockChain{
         this.chain.push(newBLock);
     }
 
+    //check the immutability of your block
     isBlockValid(){
         for(let i = 1; i < this.chain.length; i++){
             var currentBlock = this.chain[i]
@@ -63,7 +75,7 @@ let oBlock = new BlockChain()
 oBlock.addBlock(new Block(1, '2018-07-27', {data : 2}))
 oBlock.addBlock(new Block(2, '2018-08-28', {data : 12}))
 oBlock.addBlock(new Block(3, '2018-08-29', {data : 5}))
- console.log('IsValid: '+ oBlock.isBlockValid())
- oBlock.chain[1].data = {data :200}
- console.log('IsValid: '+ oBlock.isBlockValid())
- console.log(JSON.stringify(oBlock,null,4))
+ console.log('IsValid: '+ oBlock.isBlockValid()) //this will print true because block is valid
+ oBlock.chain[1].data = {data :200} //Tempering Block : changing rhe block data
+ console.log('IsValid: '+ oBlock.isBlockValid()) //this will print false because block has been tempered
+ console.log(JSON.stringify(oBlock,null,4)) //printing our whole block
---
+++ Readme.md
@@ -0,0 +1,22 @@
+## About
+- This will teach you a basic block chain algorithm. You will learn how to create a simple blockchain and how to make it immutable. In future commit, you will see how to perform transactions in blockchain.
+
+## What is Blockchain?
+- As name suggest, blockchain is a the chain of smaller blocks, which contains data. A single block has 3 important part : 
+    ###### Data : The data which block holds.
+    ###### Hash : The hash code of current block.
+    ###### Previous Hash : The hashcode of previous block, this maintain continuity.
+
+## What is GenesisBlock?
+- This is the very first block of BlockChain. You can say block chain starts from here. It does not contain the hash of previous block.
+
+## Is this Linked List?
+- Yes, You can say. It works like linked list but instead of connecting with address, It works on the base of hash of the block
+
+## What makes BlockChain immutable?
+- As any one try to tamper block chain by changing its hash or data, its hash and previous hash regenrated and a every single user has a seperate copy of blockchain which we called digital ledger, it finds out the odd man out and deny the verification process.
+
+
+## Like & Subscribe Us
+    ###### YouTube : https://www.youtube.com/martand89
+    ###### Facebook : https://www.facebook.com.codemakerz
